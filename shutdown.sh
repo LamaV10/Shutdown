74cb8bff5fdb42eec6a281682d7204320b2e8a83
LamaV10 <lama.customs@gmail.com>
2024-10-08 16:53:40 +0200

    Option added to cancel your shutdown.

--- shutdown.sh
+++ shutdown.sh
@@ -2,6 +2,7 @@ echo "Which type of shutdown do you want?"
 echo "Now (1): "
 echo "At a certain time (2): "
 echo "In a given time (3): "
+echo "Cancel shutdown (4): "
 
 read shutdown
 
@@ -19,4 +20,8 @@ if [ $shutdown -eq 3 ]; then
   echo "minutes till shutdown:"
   read time 
   sudo shutdown $time
+fi
+
+if [ $shutdown -eq 4 ]; then
+  sudo shutdown -c
 fi
